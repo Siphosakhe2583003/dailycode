bdb90dff39c6bf276ccfdf7d71d5f1a061e6b2b7
Siphosakhe2583003 <2583003@students.wits.ac.za>
2024-11-24 14:57:23 +0200

    added minimum code changes to count as a day. Needs to be tested though

--- extension.js
+++ extension.js
@@ -1,14 +1,20 @@
 const vscode = require("vscode");
 
 let counterStatusBar;
-
+let listener;
+let NUM_OF_CHANGES = 0;
 function activate(context) {
   let lastAccessDate = context.globalState.get(
     "dailycode.lastAccessDate",
     new Date()
   );
+  context.globalState.update("dailycode.lastAccessDate", lastAccessDate);
+
   let counter = context.globalState.get("dailycode.counter", 1);
   context.globalState.update("dailycode.counter", counter);
+
+  let MIN_CODE_CHANGES = context.globalState.get("dailycode.minCodeChanges", 0);
+  context.globalState.update("dailycode.minCodeChanges", MIN_CODE_CHANGES);
 
   // Create a status bar item
   if (!counterStatusBar) {
@@ -17,7 +23,32 @@ function activate(context) {
     );
     counterStatusBar.command = "dailycode.showCodingStreak";
   }
-  updateCounter(lastAccessDate, context);
+  if (MIN_CODE_CHANGES === 0) {
+    updateCounter(lastAccessDate, context);
+  } else if (
+    new Date(lastAccessDate).toDateString() !== new Date().toDateString()
+  ) {
+    listener = vscode.workspace.onDidChangeTextDocument((event) => {
+      let changes = event.contentChanges;
+
+      NUM_OF_CHANGES += changes.length;
+      vscode.window.showInformationMessage(
+        `Number of min code changes: ${MIN_CODE_CHANGES}`
+      );
+      if (NUM_OF_CHANGES >= MIN_CODE_CHANGES) {
+        console.log(`Number of changes: ${NUM_OF_CHANGES}`);
+        updateCounter(lastAccessDate, context);
+        NUM_OF_CHANGES = 0;
+        listener.dispose();
+      }
+    });
+  } else {
+    updateCounter(lastAccessDate, context);
+  }
+  if (listener) {
+    context.subscriptions.push(listener);
+  }
+
   updateStatusBar(context);
   counterStatusBar.show();
 
@@ -25,6 +56,7 @@ function activate(context) {
   context.globalState.setKeysForSync([
     "dailycode.counter",
     "dailycode.lastAccessDate",
+    "dailycode.minCodeChanges",
   ]);
 
   // Command to show coding streak
@@ -56,6 +88,57 @@ function activate(context) {
       );
       context.subscriptions.push(resetStreak);
     }
+    if (!commands.includes("dailycode.setMinCodeChanges")) {
+      const setMinCodeChanges = vscode.commands.registerCommand(
+        "dailycode.setMinCodeChanges",
+        async () => {
+          const minCodeChanges = await vscode.window.showInputBox({
+            prompt:
+              "Set the minimum number of code changes to qualify as an active day.",
+            placeHolder: "Enter a number (e.g., 10)",
+            validateInput: (value) => {
+              if (isNaN(value) || value.trim() === "") {
+                return "Please enter a valid number.(e.g., 10)";
+              }
+              if (Number(value) < 0) {
+                return "The number must be greater than or equal to 0.";
+              }
+              return null;
+            },
+          });
+
+          if (minCodeChanges) {
+            const changesNumber = Number(minCodeChanges);
+            context.globalState.update(
+              "dailycode.minCodeChanges",
+              changesNumber
+            );
+            MIN_CODE_CHANGES = changesNumber;
+            vscode.window.showInformationMessage(
+              `Success! The minimum code changes required per day are now set to ${changesNumber}.`
+            );
+          } else {
+            vscode.window.showInformationMessage(
+              "No changes were made. The minimum code changes setting remains unchanged."
+            );
+          }
+        }
+      );
+      context.subscriptions.push(setMinCodeChanges);
+    }
+    if (!commands.includes("dailycode.getMinCodeChanges")) {
+      const getMinCodeChanges = vscode.commands.registerCommand(
+        "dailycode.getMinCodeChanges",
+        () => {
+          vscode.window.showInformationMessage(
+            `The minimum code changes required per day are currently set to ${context.globalState.get(
+              "dailycode.minCodeChanges"
+            )}.`
+          );
+        }
+      );
+      context.subscriptions.push(getMinCodeChanges);
+    }
   });
 }
 
@@ -63,7 +146,9 @@ function updateStatusBar(context) {
   let counter = context.globalState.get("dailycode.counter");
 
   counterStatusBar.text = `$(flame) ${counter}`;
-  counterStatusBar.tooltip =  `${counter} day${counter > 1 ? "s" : ""} streak! 🚀`;
+  counterStatusBar.tooltip = `${counter} day${
+    counter > 1 ? "s" : ""
+  } streak! 🚀`;
 }
 
 function updateCounter(lastAccessDate, context) {
@@ -78,7 +163,7 @@ function updateCounter(lastAccessDate, context) {
     if (lastDate.toDateString() === yesterday.toDateString()) {
       counter++;
       vscode.window.showInformationMessage(
-         `Awesome! Your coding streak is now ${counter} days strong! Keep up the great work! 🔥🚀`
+        `Awesome! Your coding streak is now ${counter} days strong! Keep up the great work! 🔥🚀`
       );
     } else if (lastDate.toDateString() !== today.toDateString()) {
       counter = 1;
@@ -90,7 +175,7 @@ function updateCounter(lastAccessDate, context) {
       counter === 1
     ) {
       vscode.window.showInformationMessage(
-         "Welcome to DailyCode! 🎉 You've just started your coding streak with 1 day! Keep going strong! ✨🚀"
+        "Welcome to DailyCode! 🎉 You've just started your coding streak with 1 day! Keep going strong! ✨🚀"
       );
     } else {
       vscode.window.showInformationMessage(
